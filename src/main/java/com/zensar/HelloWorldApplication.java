21f0d77f7a5146db443733cc980a6d113b9ec39e
sharad <sharad.rajore@gmail.com>
2021-12-23 10:58:10 +0530

    added everything on repo

---
+++ src/main/java/com/zensar/HelloWorldApplication.java
@@ -0,0 +1,24 @@
+package com.zensar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class HelloWorldApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return super.configure(builder);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Ram");
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+	
+	
+
+}
